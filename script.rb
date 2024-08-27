b128a05dda2908b7f9d2f8d1c96550331d66b1b1
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-08-27 16:58:19 -0500

    Added variable that stores the ascii value of a char and created a variable to store the modified char...Char gets modified using the ascii value

--- script.rb
+++ script.rb
@@ -7,11 +7,22 @@ def caesar_cipher(text, shift)
   # New string to hold the encrypted text
   modified_text = ""
 
+  # Shift a char in the original text or do nothing if it's not a letter
   text.each_char do |char|
     if char.match(/[a-zA-z]/)
-      
+      # Convert char to ascii value (need conditional for upper and lower case values)
+      ascii_base_value = char.ord < 91 ? 'A'.ord : 'a'.ord
+
+      # Shift character through alphabet
+      modified_char = ((char.ord - ascii_base_value + shift) % 26 + ascii_base_value).chr
+
+      modified_text += modified_char
     else
       modified_text += char
     end
   end
+
+  return modified_text
 end
+
+puts caesar_cipher("Hello, World!", 3)
