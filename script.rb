a5f976dc0b497355e42a9e0da9fcb3478bbf84e5
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-08-26 20:51:16 -0500

    Created main script file with initial code included for what is needed to start setting up the caesar cipher

---
+++ script.rb
@@ -0,0 +1,21 @@
+=begin
+Implement a caesar cipher that takes in a string and 
+the shift factor and then outputs the modified string
+=end
+
+# Remember how to convert a string into a number
+# Don't forget to wrap from z to a (or a to z if shifting backwards)
+# Don't forget to keep the same casing on words
+
+alphabet = 
+[
+  "a", "b", "c", "d", "e",
+  "f", "g", "h", "i", "j",
+  "k", "l", "m", "n", "o",
+  "p", "q", "r", "s", "t",
+  "u", "v", "w", "x", "y", "z"
+]
+
+def caesar_cipher(text, shift)
+
+end
