6b17d7122ef5962efa5f4412c6f3dbf878389bc0
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-08-29 12:16:11 -0500

    Added rubocop and necessary Gemfile files to use Robocop to adjust formatting in script.rb file

--- script.rb
+++ script.rb
@@ -1,28 +1,27 @@
-=begin
-Implement a caesar cipher that takes in a string and 
-the shift factor and then outputs the modified string
-=end
+# frozen_string_literal: true
+
+# Implement a caesar cipher that takes in a string and
+# the shift factor and then outputs the modified string
 
 def caesar_cipher(text, shift)
   # New string to hold the encrypted text
-  modified_text = ""
+  modified_text = ''
 
   # Shift a char in the original text or do nothing if it's not a letter
   text.each_char do |char|
-    if char.match(/[a-zA-z]/)
+    if char.match(/[a-zA-Z]/)
       # Convert char to ascii value (need conditional for upper and lower case values)
       ascii_base_value = char.ord < 91 ? 'A'.ord : 'a'.ord
 
       # Shift character through alphabet
       modified_char = ((char.ord - ascii_base_value + shift) % 26 + ascii_base_value).chr
-
       modified_text += modified_char
     else
       modified_text += char
     end
   end
 
-  return modified_text
+  modified_text
 end
 
-puts caesar_cipher("Hello, World!", 3)
+puts caesar_cipher('Hello, World!', 3)
